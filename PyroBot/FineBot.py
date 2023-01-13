5cfdfcf1c27621d44981a165a8145285ee2ec111
Eyuel Solomon <110692570+FineGuy2@users.noreply.github.com>
2023-01-13 22:34:15 +0300

    Few Commands added

--- PyroBot/FineBot.py
+++ PyroBot/FineBot.py
@@ -4,12 +4,33 @@ bot = Client(
     "FirstGo" ,
     api_id = 16513149,
     api_hash = "2f141cc5bbae5c37d0ed1c1b8a72c280",
-    bot_token="5818099862:AAEJXeqJI7dApcWZXddmaNZCTw6cwvK_TLo"
+    bot_token="5836107188:AAG21vssOh_3emRT52Ivi6JJXwoQ-bANkqc"
 )
 @bot.on_message(filters.command('start')& filters.private)
 
 def command1(bot, message):
     bot.send_message(message.chat.id ,"Hello ")
+
+@bot.on_message(filters.command('greet'))
+
+def command1(bot, message):
+    bot.send_message(message.chat.id ,"Hello There")
+
+@bot.on_message(filters.command('help'))
+
+def command1(bot, message):
+    bot.send_message(message.chat.id ,"/greet  - Greets the person who sends greet   ")
+
+@bot.on_message(filters.command('who'))
+
+def command1(bot, message):
+    message.reply_text("I am FineBot")
+
+@bot.on_message(filters.text & filters.private)
+
+def command1(Client, message):
+    message.reply_text(message.text)
+
 
 print("bot is working")
 bot.run()   
